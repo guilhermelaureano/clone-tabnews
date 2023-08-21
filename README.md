03d04f53961ca188193365f1dfc8d686f2c5c865
Guilherme Laureano <39038762+guilhermelaureano@users.noreply.github.com>
2023-08-21 20:21:57 +0000

    add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto criado para estudo/ treinamento. Trata-se da implementação do https://www.tabnews.com.br para o curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1>"A coragem não é ausência do medo; é a persistência apesar do medo."</h1>;
+function Home() {
+  return (
+    <h1>
+      "A coragem não é ausência do medo; é a persistência apesar do medo."
+    </h1>
+  );
 }
 
 export default Home;
